f04aae396729cff1edb75a93828f7b7b3720e53f
ioubairi <ismail.oubairi@capgemini.com>
2020-01-13 11:26:06 +0100

    refactor git hub api

---
+++ app/src/main/java/com/github/toprepo/network/GithubService.kt
@@ -0,0 +1,12 @@
+package com.github.toprepo.network
+
+import com.github.toprepo.models.GitHubApiResult
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface GithubService {
+    @GET("repositories")
+    suspend fun getTop(
+        @QueryMap options: HashMap<String, String>) : Response<GitHubApiResult>
+}
